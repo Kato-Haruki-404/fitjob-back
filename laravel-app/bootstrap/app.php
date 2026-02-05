f8f2fcf3c1cbde258f3e2c8cb11bc48000fa0236
funa-dev <funahashi.ryouma@gmail.com>
2026-02-05 10:48:53 +0900

    [fix]投稿できない問題修正。

--- laravel-app/bootstrap/app.php
+++ laravel-app/bootstrap/app.php
@@ -15,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->statefulApi();
+        // $middleware->statefulApi();
         // APIルート全体にCamelCaseResponseを適用
         $middleware->api(append: [
             CamelCaseResponse::class,
